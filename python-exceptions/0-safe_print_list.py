556530f72b169fca852f34b4019c42eeefef90ce
Noumso <noumane.82300@gamil.com>
2025-01-24 20:29:44 +0000

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    idx = 0
-    while idx < x:
-        try:
-            print("{}".format(my_list[idx]), end="")
-        except IndexError:
-            break
-        idx += 1
-    print("")
-    return idx
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
